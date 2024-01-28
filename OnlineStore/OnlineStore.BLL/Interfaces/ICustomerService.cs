68bbd52f99f8c70885bd5710d81ee8c6518eb572
m_bubela <m_bubela@mifprojects.com>
2024-01-28 16:57:26 +0300

    Deleted predicate from Customer interface

--- OnlineStore/OnlineStore.BLL/Interfaces/ICustomerService.cs
+++ OnlineStore/OnlineStore.BLL/Interfaces/ICustomerService.cs
@@ -1,11 +1,4 @@
 ﻿using OnlineStore.BLL.DTO;
-using OnlineStore.DAL.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OnlineStore.BLL.Interfaces
 {
@@ -16,7 +9,5 @@ namespace OnlineStore.BLL.Interfaces
         Task<CustomerDTO> GetCustomerByIdAsync(long id);
         Task<CustomerDTO> GetCustomerByUsernameAsync(string username);
         Task<List<CustomerDTO>> GetCustomersListAsync();
-
-        Expression<Func<Customer, bool>> CreateActiveCustomerPredicate(long customerId);
     }
 }
